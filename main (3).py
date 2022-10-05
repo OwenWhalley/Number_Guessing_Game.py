bafcca23a2f306d3defbf1d1f366ae26c5104432
OwenWhalley <113034255+OwenWhalley@users.noreply.github.com>
2022-10-05 09:19:43 -0400

    Delete main (3).py

--- main (3).py
+++
@@ -1,8 +0,0 @@
-from random import *
-a = randint(1, 6)
-
-guess = int(input("Select a number between 1 and 6:"))
-if a == guess:
-  print("You guessed right!")
-else:
-  print("The answer is: %d" %(a))
